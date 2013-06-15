f2d3bc8003dc24819ffca156dd217b2ce3a8dfec
umakoz <umami@inf9.co.jp>
2013-06-15 17:29:23 +0900

    RSpecのスペックを日本語に変更した

--- spec/lib/zip_spec.rb
+++ spec/lib/zip_spec.rb
@@ -4,25 +4,30 @@ require 'spec_helper'
 describe Zip do
   # let(:sample){ Sample.new }
 
-  it "blank" do
+  it "空欄のとき" do
     Zip.new.valid?("").should be_false
   end
-  it "niketa" do
+
+  it "２桁の数字のとき" do
     Zip.new.valid?("11").should be_false
   end
-  it "sanketa" do
+
+  it "３桁の数字のとき" do
     Zip.new.valid?("111").should be_true
     Zip.new.valid?("000").should be_true
   end
-  it "nanaketa" do
+
+  it "７桁の数字のとき" do
     Zip.new.valid?("1234567").should be_true
     Zip.new.valid?("8765432").should be_true
   end
-  it "-8keta" do
+
+  it "○○○-○○○○のとき" do
     Zip.new.valid?("123-4567").should be_true
     Zip.new.valid?("876-5432").should be_true
   end
-  it "alpha" do
+
+  it "アルファベットが含まれている時" do
     Zip.new.valid?("abc-4567").should be_false
     # Zip.new.valid?("876-5432").should be_false
   end
