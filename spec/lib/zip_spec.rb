45253f75c3e3472c4191137159103241908e3ff7
umakoz <umami@inf9.co.jp>
2013-06-15 17:37:06 +0900

    インスタンスの生成をまとめた

--- spec/lib/zip_spec.rb
+++ spec/lib/zip_spec.rb
@@ -2,37 +2,37 @@
 require 'spec_helper'
 
 describe Zip do
-  # let(:sample){ Sample.new }
+  let(:zip){ Zip.new }
 
   it "空欄のとき" do
-    Zip.new.valid?("").should be_false
+    zip.valid?("").should be_false
   end
 
   it "２桁の数字のとき" do
-    Zip.new.valid?("11").should be_false
+    zip.valid?("11").should be_false
   end
 
   it "３桁の数字のとき" do
-    Zip.new.valid?("111").should be_true
-    Zip.new.valid?("000").should be_true
+    zip.valid?("111").should be_true
+    zip.valid?("000").should be_true
   end
 
   it "７桁の数字のとき" do
-    Zip.new.valid?("1234567").should be_true
-    Zip.new.valid?("8765432").should be_true
+    zip.valid?("1234567").should be_true
+    zip.valid?("8765432").should be_true
   end
 
   it "○○○-○○○○のとき" do
-    Zip.new.valid?("123-4567").should be_true
-    Zip.new.valid?("876-5432").should be_true
+    zip.valid?("123-4567").should be_true
+    zip.valid?("876-5432").should be_true
   end
 
   it "アルファベットが含まれている時" do
-    Zip.new.valid?("abc-4567").should be_false
-    Zip.new.valid?("876-defg").should be_false
+    zip.valid?("abc-4567").should be_false
+    zip.valid?("876-defg").should be_false
   end
 
   it "ハイフンの位置が間違っている時" do
-    Zip.new.valid?("1234-567").should be_false
+    zip.valid?("1234-567").should be_false
   end
 end
